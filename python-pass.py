a0ca0f436a78482415acc9bb9a6b0384d8924f31
Zubaidah Bilal <zubaida.bilal98@gmail.com>
2021-09-30 18:14:52 +0300

    Python Task Solution

--- python-pass.py
+++ python-pass.py
@@ -4,4 +4,31 @@ class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        palindromic = ''
+
+        # loop through the input string
+        for i in range(len(s)):
+
+            # loop backwards through the input string
+            for j in range(len(s), i, -1):
+
+                # Break if out of range
+                if len(palindromic) >= j-i:
+                    break
+
+                # Update variable if matches
+                elif s[i:j] == s[i:j][::-1]:
+                    palindromic = s[i:j]
+                    break
+
+        return palindromic
+
+
+# test command: 
+print(Solution.longest_palindromic('babad')) # >> bab
+print(Solution.longest_palindromic('cbbd'))  # >> bb
+print(Solution.longest_palindromic('a'))     # >> a
+print(Solution.longest_palindromic('ac'))    # >> a
+
+
+
